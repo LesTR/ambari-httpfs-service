50953d1a2d431b78dd89eecb53ec1b2086774010
Martin Bohacek <martin.bohacek@merck.com>
2016-11-18 14:43:06 +0100

    Fnalizing HTTPFS component * Enable automated kerberization * Add kerberos support for service check

--- src/services/HTTPFS/package/scripts/params.py
+++ src/services/HTTPFS/package/scripts/params.py
@@ -1,17 +1,23 @@
 from resource_management import *
+from resource_management.libraries.functions import get_kinit_path
+
 config = Script.get_config()
+tmp_dir = Script.get_tmp_dir()
 
 ignore_groupsusers_create = default("/configurations/cluster-env/ignore_groupsusers_create", False)
 smoke_user                = default("/configurations/cluster-env/smokeuser","ambari-qa")
+smoke_user_principal      = default("/configurations/cluster-env/smokeuser_principal_name","")
+smoke_user_keytab         = default("/configurations/cluster-env/smokeuser_keytab", "")
+kinit_path_local          = get_kinit_path(default("/configurations/kerberos-env/executable_search_paths", None))
 
 httpfs_user               = config['configurations']['httpfs']['user']
 httpfs_group              = config['configurations']['httpfs']['group']
 httpfs_conf_dir           = config['configurations']['httpfs']['conf_dir']
 
+httpfs_server_host        = config['clusterHostInfo']['httpfs_gateway_hosts'][0]
 httpfs_server_port        = config['configurations']['httpfs']['port']
 httpfs_server_admin_port  = config['configurations']['httpfs']['admin.port']
 httpfs_log_dir            = config['configurations']['httpfs']['httpfs_log_dir']
 
 httpfs_env_template       = config['configurations']['httpfs-env']['content']
 httpfs_log4j_content      = config['configurations']['httpfs-log4j']['content']
-
--- src/services/HTTPFS/package/scripts/service_check.py
+++ src/services/HTTPFS/package/scripts/service_check.py
@@ -1,13 +1,22 @@
 from resource_management import *
+from resource_management.libraries.functions.curl_krb_request import curl_krb_request
 
 class HttpFS_service_check(Script):
     def service_check(self, env):
         import params
         env.set_params(params)
         Logger.info("Checking HttpFS service")
-        #@TODO: find better check
-        Execute(format("curl 'http://127.0.0.1:{params.httpfs_server_port}/webhdfs/v1/tmp/?op=liststatus&user.name={params.smoke_user}'"))
-        Logger.info("Checking HttpFS service - DONE")
+
+        uri = format("http://{params.httpfs_server_host}:{params.httpfs_server_port}/webhdfs/v1/tmp/?op=LISTSTATUS")
+        response, errmsg, time_millis = curl_krb_request(params.tmp_dir, params.smoke_user_keytab,
+                                                         params.smoke_user_principal, uri, "httpfs_service_check",
+                                                         params.kinit_path_local, False, None, params.smoke_user)
+
+        if not response:
+            Logger.error("Cannot access HttpFS on: {0}. Error : {1}".format(uri, errmsg))
+            return 1
+
+        Logger.info("Checking HttpFS service ({0}ms) - DONE".format(time_millis))
 
 if __name__ == "__main__":
     HttpFS_service_check().execute()
